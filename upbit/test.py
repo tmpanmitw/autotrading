7fc90db69c4c88af2a631f93c4a83986b88e9d58
tmpanmitw <112293743+tmpanmitw@users.noreply.github.com>
2023-08-17 18:09:08 +0900

    #1 test
    #1 test

---
+++ upbit/test.py
@@ -0,0 +1,9 @@
+import pyupbit
+
+access = "aaaa"          # 본인 값으로 변경
+secret = "bbbb"          # 본인 값으로 변경
+upbit = pyupbit.Upbit(access, secret)
+
+print(upbit.get_balance("KRW-XRP"))     # KRW-XRP 조회
+print(upbit.get_balance("KRW-BTC"))     # KRW-XRP 조회
+print(upbit.get_balance("KRW"))         # 보유 현금 조회
